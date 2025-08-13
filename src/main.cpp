fd811cfc9fe72b1896e33b7f9d29705c45304be3
sarbesh0x01 <weebmaniac2314@gmail.com>
2025-08-13 18:31:40 +0545

    Finishing the initial setup

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,12 @@ int main() {
       crow::mustache::set_base("./templates/");
   // variable to store a data  //
   // To Optimize load essentioal tings to memory
-  CROW_ROUTE(app, "/<string>")
-  ([](std::string name) {
+  CROW_ROUTE(app, "/")
+  ([]() {
     auto page = crow::mustache::load(
         "index.html");
-    // res.set_static_file_info(
-    //     "/home/sarbesh/Projects/WebsiteDevelopment/index.html");
-    //  res.set_header("content-type", "text/html");
-    // res.end();
-    // return (200, "text/html", "data");
-    crow::mustache::context ctx({{"person", name}});
-    return page.render(ctx);
+    return page.render();
   });
-
   app.port(18000).run();
 }
 
